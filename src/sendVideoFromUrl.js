c0a92e67ea329471e3d681ef73e1b207ee91422c
rayhanalive <mahmudxyz007@gmail.com>
2026-09-12 07:03:07 +0600

    feat: add src/sendVideoFromUrl.js

---
+++ src/sendVideoFromUrl.js
@@ -0,0 +1,6 @@
+'use strict';
+const { invoke, optionsCallback } = require('./api-helpers');
+module.exports = (defaults, api, ctx) => (threadID, url, options, callback) => {
+  const parsed = optionsCallback(options, callback);
+  return invoke(ctx, ctx.client.sendVideoFromUrl, [threadID, url, parsed.options], parsed.callback);
+};
